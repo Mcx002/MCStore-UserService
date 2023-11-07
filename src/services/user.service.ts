6919eddf075ddd97709d21107ce82457bff1bcf1
Mcx002 <muchlishchoeruddin96@gmail.com>
2023-11-07 17:08:15 +0700

    Add create user on service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -3,7 +3,7 @@ import { UserDto } from '../../proto_gen/user_pb'
 import { ErrorHandler } from '../adapter/error.adapter'
 import { App } from '../app'
 import { BaseService } from '../interfaces/service.interface'
-import { UserAttributes } from '../models/user.model'
+import { UserAttributes, UserCreationAttributes } from '../models/user.model'
 import { UserRepository } from '../repositories/user.repository'
 import { getUnixFromDate } from '../utils/time'
 
@@ -14,9 +14,9 @@ export class UserService extends BaseService {
         this.userRep = app.repository.userRepository
     }
 
-    getUser = async (xid: string): Promise<UserDto> => {
+    getUser = async (id: string): Promise<UserDto> => {
         // find user
-        const user = await this.userRep.findUserByXid(xid)
+        const user = await this.userRep.findUserById(id)
         if (!user) {
             throw new ErrorHandler(Status.INVALID_ARGUMENT, "User is not found")
         }
@@ -24,9 +24,37 @@ export class UserService extends BaseService {
         return this.composeUserDto(user)
     }
 
+    create = async (userDto: UserDto): Promise<UserDto> => {
+        // check existing email
+        let user = await this.userRep.findUserByEmail(userDto.getEmail())
+        if (user) {
+            throw new ErrorHandler(Status.INVALID_ARGUMENT, "The email has been used")
+        }
+
+        // prepare user creation attributes
+        const userData: UserCreationAttributes = {
+            createdAt: new Date(),
+            updatedAt: new Date(),
+            firstName: userDto.getFirstName(),
+            lastName: userDto.getLastName(),
+            email: userDto.getEmail(),
+            birthday: new Date(userDto.getBirthday()),
+            gender: userDto.getGender(),
+            phone: userDto.getPhone(),
+            address: userDto.getAddress(),
+            version: 1,
+        }
+
+        // execute
+        user = await this.userRep.insert(userData)
+
+        // compose result
+        return this.composeUserDto(user)
+    }
+
     composeUserDto = (user: UserAttributes): UserDto => {
         const userDto = new UserDto()
-        userDto.setXid(user.xid)
+        userDto.setId(user.id)
         userDto.setEmail(user.email)
         userDto.setPhone(user.phone)
         userDto.setGender(user.gender)
@@ -34,7 +62,7 @@ export class UserService extends BaseService {
         userDto.setVersion(user.version)
         userDto.setBirthday(getUnixFromDate(user.birthday))
         userDto.setFirstName(user.firstName)
-        userDto.setLastName(user.lastName)
+        userDto.setLastName(user.lastName ?? '')
         userDto.setCreatedAt(getUnixFromDate(user.createdAt))
         userDto.setUpdatedAt(getUnixFromDate(user.updatedAt))
         return userDto
--- tests/unit/services/user.service.test.ts
+++ tests/unit/services/user.service.test.ts
@@ -1,30 +1,89 @@
+import { UserDto } from '../../../proto_gen/user_pb'
 import { App } from '../../../src/app'
 import { UserAttributes } from '../../../src/models/user.model'
+import { v4 as uuidv4 } from 'uuid'
 
 describe('Service User.getUser Test', () => {
     test('#SUT1 Should throw user is not found', async () => {
         const app = new App()
         app.service.init(app)
 
-        jest.spyOn(app.repository.userRepository, "findUserByXid").mockImplementation(async () => null)
+        jest.spyOn(app.repository.userRepository, "findUserById").mockImplementation(async () => null)
 
         expect(() => app.service.userService.getUser('test')).rejects.toThrow('User is not found')
     })
 
-    test('#SUT2 Should return user', async () => {
+    test('#SUT2 Should return user without lastName available', async () => {
         const app = new App()
         app.service.init(app)
 
-        const xid = 'test'
-        jest.spyOn(app.repository.userRepository, "findUserByXid").mockImplementation(async () => ({
-            xid,
+        const id = uuidv4()
+        jest.spyOn(app.repository.userRepository, "findUserById").mockImplementation(async () => ({
+            id,
             birthday: new Date(),
             createdAt: new Date(),
             updatedAt: new Date(),
         } as UserAttributes))
 
-        const userDto = await app.service.userService.getUser(xid)
+        const userDto = await app.service.userService.getUser(id)
 
-        expect(userDto.getXid()).toBe(xid)
+        expect(userDto.getId()).toBe(id)
+    })
+
+    test('#SUT3 Should return user wit lastName available', async () => {
+        const app = new App()
+        app.service.init(app)
+
+        const id = uuidv4()
+        jest.spyOn(app.repository.userRepository, "findUserById").mockImplementation(async () => ({
+            id,
+            lastName: 'userLastName',
+            birthday: new Date(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        } as UserAttributes))
+
+        const userDto = await app.service.userService.getUser(id)
+
+        expect(userDto.getId()).toBe(id)
+    })
+})
+
+describe('Service User.create Test', () => {
+    test('#SUT4 Should throw The email has been used', async () => {
+        const app = new App()
+        app.service.init(app)
+
+        const id = uuidv4()
+        jest.spyOn(app.repository.userRepository, "findUserByEmail").mockImplementation(async () => ({
+            id,
+            birthday: new Date(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        } as UserAttributes))
+
+        const userDto = new UserDto()
+        userDto.setEmail('user@test.com')
+        expect(() => app.service.userService.create(userDto)).rejects.toThrow("The email has been used")
+    })
+
+    test('#SUT5 Should return user', async () => {
+        const app = new App()
+        app.service.init(app)
+
+        const id = uuidv4()
+        jest.spyOn(app.repository.userRepository, "findUserByEmail").mockImplementation(async () => null)
+        jest.spyOn(app.repository.userRepository, "insert").mockImplementation(async () => ({
+            id,
+            birthday: new Date(),
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        } as UserAttributes))
+
+        const userDto = new UserDto()
+        userDto.setEmail('user@test.com')
+
+        const user = await app.service.userService.create(userDto)
+        expect(user.getId()).toBe(id)
     })
 })
